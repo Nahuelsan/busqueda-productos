9b857b02108195db0a7c09f1158c66a4e3f878d7
Nahuelsan <nahuel.-sanchez@live.com.ar>
2021-01-03 08:31:57 -0300

    Heroku

--- api/src/app.js
+++ api/src/app.js
@@ -7,7 +7,7 @@ const server = express();
 server.name = 'API';
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); 
+  res.header('Access-Control-Allow-Origin', '*'); 
   next();
 });
 

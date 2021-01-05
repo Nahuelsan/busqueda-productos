8aec30097d05a994581a3610c948327d09e19084
Nahuelsan <nahuel.-sanchez@live.com.ar>
2021-01-04 22:49:36 -0300

    Deployando

---
+++ client/build/precache-manifest.99d311f6fbd7e593274d1ecf82044a69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbab49c425ba938b7c29239281108f85",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d3ea2a26fab7f5680c2",
+    "url": "/static/css/main.c472b830.chunk.css"
+  },
+  {
+    "revision": "90717c9b480fc40451ae",
+    "url": "/static/js/2.e8ec0de9.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.e8ec0de9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d3ea2a26fab7f5680c2",
+    "url": "/static/js/main.92ed7bc5.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
